9912582461f3dacd4435d5eaf474f4131c13cfc2
Michael Stramel <m.stramel89@gmail.com>
2019-10-01 23:30:28 -0500

    try each pack on a separate job

--- packages/styled-icons/integration/parallelize.js
+++ packages/styled-icons/integration/parallelize.js
@@ -5,7 +5,8 @@ const {JOB_COUNT = 1, JOB_INDEX = 0} = process.env
 
 function getFiles() {
   const allFiles = fg.sync('tests/**/*.test.js', {cwd: __dirname})
-  const filesPerJob = Math.ceil(allFiles.length / JOB_COUNT)
+  const filesPerJob =
+    Math.floor(allFiles.length / JOB_COUNT) + (allFiles.length % JOB_COUNT > JOB_INDEX ? 1 : 0)
   const startIndex = filesPerJob * JOB_INDEX
   return allFiles.slice(startIndex, startIndex + filesPerJob)
 }
